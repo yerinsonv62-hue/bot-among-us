f9e158c60c5a82150c6a9659eb0625c204270c11
yerinsonv62-hue <yerinsonv62@gmail.com>
2026-09-10 22:25:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -218,4 +218,8 @@ client.on("interactionCreate", async interaction => {
   }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+// Reemplaza esto:
+// client.login(process.env.DISCORD_TOKEN);
+
+// Por esto (colocando tu token dentro de las comillas):
+client.login("MTU0Nzc4NjQzOTEyMTU2NzgzNA.G4FI-q.BvtY6PPtTUkqEPYL4fgONVKEEqrA3YHx6-NJCQ");
